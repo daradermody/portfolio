2d81ce6dcd28b3667c32ed289faeccd9d3b46af7
Dara Dermody <dara.dermody@ericsson.com>
2018-06-07 13:44:56 +0100

    Added footer

--- src/app/paralax-corners/paralax-corners.component.ts
+++ src/app/paralax-corners/paralax-corners.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-paralax-corners',
   templateUrl: './paralax-corners.component.html',
   styleUrls: ['./paralax-corners.component.css']
 })
-export class ParalaxCornersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class ParalaxCornersComponent {}
